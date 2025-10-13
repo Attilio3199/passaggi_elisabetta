c900b6852de56ef09e8ece0e302d77534d54f0a3
attiliobut.gdv <attiliobut.gdv@gmail.com>
2025-10-13 11:06:32 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/ResetWidget/ResetWidget.js
@@ -0,0 +1,22 @@
+export default {
+  resetWidgets: () => {
+    resetWidget("PersoneInNegozio");
+    resetWidget("PrimaGioielleria");
+    resetWidget("PrimaOreficeria");
+    resetWidget("PrimaArgento");
+    resetWidget("PrimaBrand");
+    resetWidget("PrimaOrologi");
+    resetWidget("PrimaOrdineVetrine");
+    resetWidget("PrimaEspositori");
+    resetWidget("PrimaPulizia");
+    resetWidget("PrimaOrdineRetro");
+    resetWidget("DopoGioielleria");
+    resetWidget("DopoOreficeria");
+    resetWidget("DopoArgento");
+    resetWidget("DopoBrand");
+    resetWidget("DopoOrologi");
+    resetWidget("DopoEspositori");
+    resetWidget("SegnalazioniAlTeam");
+    resetWidget("OsservazioniAlTeam");
+  }
+}
